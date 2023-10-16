fcb36d62574e70ac046c9b0f139f60348ee29cfe
Santiago Torricella <torrisantiago@gmail.com>
2023-10-16 13:43:21 -0300

    Start cv-header update on screen

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable react/react-in-jsx-scope */
+import { useState } from "react";
 
 function CVPreview() {
+  const [name, setName] = useState("");
   return (
     <div className="cv-sheet">
-      <div className="cv-header"></div>
+      <div className="cv-header">
+        <div>
+          <p>{name}</p>
+        </div>
+      </div>
       <div className="cv-education"></div>
       <div className="cv-experience"></div>
     </div>
