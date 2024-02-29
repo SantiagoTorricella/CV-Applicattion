1fb94b3c519a4c15d80e9fef201bd811ce3c682f
Santiago <torrisantiago@gmail.com>
2024-02-28 23:05:35 -0300

    EducaitonCard initiation

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -1,3 +1,12 @@
-function EducationCard() {}
+/* eslint-disable require-jsdoc */
+/* eslint-disable react/react-in-jsx-scope */
+
+function EducationCard(education) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
 
 export default EducationCard;
